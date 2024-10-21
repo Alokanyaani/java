fe20eb9fcd6ef6e4cda69b1fb7becd1c7924f28b
Alokanyaani <alokanyaani@gmail.com>
2024-10-21 21:58:47 +0530

    java program

--- Practice/prt5.java
+++ Practice/prt5.java
@@ -5,6 +5,20 @@
 
 package Practice;
 
+
+import java.util.Scanner;
+
 public class prt5 {
+    public  static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("first num:");
+        int A = sc.nextInt();
+
+
+        System.out.println("second num:");
+        int B =sc.nextInt(); 
+        
+        System.out.println("A+B-:" +( A*B));
+    } 
     
 }
